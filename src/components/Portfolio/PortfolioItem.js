1656be3e89d1ce64391d611f663e93aacab3ba54
Venelin Nikolov <venelinn@gmail.com>
2024-02-12 09:49:34 -0500

    some tweaks

--- src/components/Portfolio/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem.js
@@ -5,6 +5,7 @@ import { animated } from 'react-spring';
 import './PortfolioItem.scss';
 
 const PortfolioItem = ({ data, style }) => {
+  const yearFromString = data.date?.split('-')[0];
   const image = getImage(data.cover);
   return (
     <animated.div style={style} className='folio'>
@@ -12,6 +13,7 @@ const PortfolioItem = ({ data, style }) => {
       <span className='folio__item'>
         <span className='folio__item__cell'>
           <h3 className='folio__item__title'>{data.title}</h3>
+          {yearFromString && <span className='folio__item__date'>{yearFromString}</span>}
         </span>
       </span>
       <Link className='folio__link' to={`/${data.slug}`}>
